d88d108d3c3dd90b13b583aca58fa1ed263ebe11
lauralardies <lrodrlar@myuax.com>
2022-09-23 17:12:06 +0200

    4 a medias

--- tabla.py
+++ tabla.py
@@ -9,8 +9,9 @@ def main(n, m):
         print("Nuestra script necesita que se introduzcan dos números, el primero que indica el número de filas de nuestra tabla y el segundo el número de columnas.")
         exit()
 
-    for i in range(n):
-        for j in range(m):
+    for i in range(n-1):
+        print("*", end='\n')
+        for j in range(m-1):
             print("*", end='')
 
 if __name__ == '__main__':
